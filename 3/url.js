fe612ec42152e37596d579363af828d786a89f92
김휘수 <qkqhrkxdkdy@naver.com>
2024-09-23 16:11:05 +0900

    url모듈 사용법

---
+++ 3/url.js
@@ -0,0 +1,6 @@
+const url = require('url');
+
+const {URL} = url;
+const myURL = new URL ('https://velog.io/@rodzlen/Node.js-%EC%9E%A5%EB%8B%A8%EC%A0%90');
+console.log('new URL(): ',myURL);
+console.log('url.format(): ',url.format(myURL));
